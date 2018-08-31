b9c409303efa2f7eb455e989b683eb581d6ad9ed
Jason Viger <jason.viger@gmail.com>
2018-08-31 12:11:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
-    erb :index
-  end
+ 
   get '/recipes/new' do
     erb :new
   end
